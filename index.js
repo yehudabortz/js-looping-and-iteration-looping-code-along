759c95a3b4776effd271124bf7c24671b69e1b95
Yehuda Bortz <yehudabortz@icloud.com>
2021-03-14 17:38:52 -0500

    commit tree f264ec7f17e1ad0a5d5d7e56604b9cbd96e7b3e0

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let thankYouCards = []
+    for (let i = 0; i < names.length; i++) {
+        thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYouCards
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number)
+        number--
+    }
+}
